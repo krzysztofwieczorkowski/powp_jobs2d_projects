f98b68899bf5e487682af62359b5040f5c4e5ddc
michal.czarnecki <michal.czarnecki@beefee.co.uk>
2026-01-15 21:29:53 +0100

    Displaying canvas in drawer panel

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dApp.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dApp.java
@@ -17,9 +17,11 @@ import edu.kis.powp.jobs2d.drivers.DriverComposite;
 import edu.kis.powp.jobs2d.drivers.adapter.LineDriverAdapter;
 import edu.kis.powp.jobs2d.visitor.VisitableJob2dDriver;
 import edu.kis.powp.jobs2d.events.*;
+import edu.kis.powp.jobs2d.features.CanvasFeature;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.jobs2d.canvas.CanvasFactory;
 
 import edu.kis.powp.jobs2d.drivers.transformation.DriverFeatureFactory;
 
@@ -117,6 +119,19 @@ public class TestJobs2dApp {
     }
 
     /**
+     * Setup canvas options.
+     * 
+     * @param application Application context.
+     */
+    private static void setupCanvases(Application application) {
+        CanvasFeature.addCanvas("None", null);
+        CanvasFeature.addCanvas(CanvasFactory.createA4());
+        CanvasFeature.addCanvas(CanvasFactory.createA3());
+        CanvasFeature.addCanvas(CanvasFactory.createB4());
+        CanvasFeature.addCanvas(CanvasFactory.createCircle(200));
+    }
+
+    /**
      * Setup menu for adjusting logging settings.
      * 
      * @param application Application context.
@@ -143,10 +158,12 @@ public class TestJobs2dApp {
             public void run() {
                 Application app = new Application("Jobs 2D");
                 DrawerFeature.setupDrawerPlugin(app);
+                CanvasFeature.setupCanvasPlugin(app);
                 CommandsFeature.setupCommandManager();
 
                 DriverFeature.setupDriverPlugin(app);
                 setupDrivers(app);
+                setupCanvases(app);
                 setupPresetTests(app);
                 setupCommandTests(app);
                 setupLogger(app);
